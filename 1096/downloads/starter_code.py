93cefb298074282dcfaf3ea956793ecfca6ecca6
Ari Blondal <ari.blondal@gmail.com>
2023-10-25 23:07:32 -0700

    Added file input problem

---
+++ 1096/downloads/starter_code.py
@@ -0,0 +1,11 @@
+inp = open("input.txt", "r").read()
+
+# Get each number of the input
+nums = [int(w) for w in inp.split()]
+
+# Do calculations
+# Your code can go here
+
+print(3) # incorrect
+
+# HI
